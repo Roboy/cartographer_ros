4a13d654dcbbe4081b787aaebe21c2ba9b743361
Chris <34374252+christqoh@users.noreply.github.com>
2018-12-07 15:01:32 +0100

    reverted file to old syntax, demo working now

--- cartographer_ros/configuration_files/backpack_2d_localization.lua
+++ cartographer_ros/configuration_files/backpack_2d_localization.lua
@@ -14,9 +14,10 @@
 
 include "backpack_2d.lua"
 
-TRAJECTORY_BUILDER.pure_localization_trimmer = {
-  max_submaps_to_keep = 3,
-}
-POSE_GRAPH.optimize_every_n_nodes = 20
+TRAJECTORY_BUILDER.pure_localization = true
+--TRAJECTORY_BUILDER.pure_localization_trimmer = {
+--  max_submaps_to_keep = 3,
+--}
+POSE_GRAPH.optimize_every_n_nodes = 1
 
 return options
