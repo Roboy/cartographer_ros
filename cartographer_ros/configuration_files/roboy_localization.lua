81c603236313cbb4067036892f4fc1ba2cf8663f
Chris <34374252+christqoh@users.noreply.github.com>
2018-12-07 14:59:41 +0100

    live localization working with ourdated syntax now and not really real time

--- cartographer_ros/configuration_files/roboy_localization.lua
+++ cartographer_ros/configuration_files/roboy_localization.lua
@@ -15,10 +15,10 @@
 include "roboy_indoor.lua"
 
 TRAJECTORY_BUILDER.pure_localization = true
---TRAJECTORY_BUILDER.pure_localization_trimmer = {
+--TRAJECTORY_BUILDER_2D.pure_localization_trimmer = {
 --	max_submaps_to_keep = 3,
 --}
-POSE_GRAPH.optimize_every_n_nodes = 2
+POSE_GRAPH.optimize_every_n_nodes = 1
 
 return options
 
