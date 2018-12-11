52e5bb540377fbb64504f98b336c6ae912345203
Chris <34374252+christqoh@users.noreply.github.com>
2018-12-11 13:13:19 +0100

    commented out some lines

--- cartographer_ros/configuration_files/roboy_localization.lua
+++ cartographer_ros/configuration_files/roboy_localization.lua
@@ -18,10 +18,12 @@ TRAJECTORY_BUILDER.pure_localization_trimmer = {
 	max_submaps_to_keep = 3,
 }
 POSE_GRAPH.optimize_every_n_nodes = 10
-POSE_GRAPH.constraint_builder.global_localization_min_score = 0.5
+--POSE_GRAPH.constraint_builder.global_localization_min_score = 0.5
+
+--POSE_GRAPH.constraint_builder.min_score = 0.7
 
 --TRAJECTORY_BUILDER_2D.max_range = 70
-POSE_GRAPH.constraint_builder.max_constraint_distance = 25
+--POSE_GRAPH.constraint_builder.max_constraint_distance = 25
 
 return options
 
