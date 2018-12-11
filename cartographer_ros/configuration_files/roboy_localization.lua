54fc10f8f2b423c1bc3fee541681041d55eef742
Chris <34374252+christqoh@users.noreply.github.com>
2018-12-11 11:23:08 +0100

    working with old syntax

--- cartographer_ros/configuration_files/roboy_localization.lua
+++ cartographer_ros/configuration_files/roboy_localization.lua
@@ -15,7 +15,7 @@
 include "roboy_indoor.lua"
 
 TRAJECTORY_BUILDER.pure_localization = true
---TRAJECTORY_BUILDER_2D.pure_localization_trimmer = {
+--TRAJECTORY_BUILDER.pure_localization_trimmer = {
 --	max_submaps_to_keep = 3,
 --}
 POSE_GRAPH.optimize_every_n_nodes = 1
