7c8eb83af50286cad20c39cfd5879fd13e86195a
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-18 10:27:10 +0100

    finetuning localization

--- cartographer_ros/configuration_files/roboy_localization.lua
+++ cartographer_ros/configuration_files/roboy_localization.lua
@@ -22,12 +22,12 @@ TRAJECTORY_BUILDER.pure_localization = true
 
 --POSE_GRAPH.constraint_builder.global_localization_min_score = 0.55
 
-POSE_GRAPH.constraint_builder.min_score = 0.7
+POSE_GRAPH.constraint_builder.min_score = 0.65
 
 --POSE_GRAPH.max_num_final_iterations = 1
 
-POSE_GRAPH.constraint_builder.max_constraint_distance = 25
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 25
+POSE_GRAPH.constraint_builder.max_constraint_distance = 10
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 10
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(180.0)
 
 POSE_GRAPH.optimize_every_n_nodes = 2
