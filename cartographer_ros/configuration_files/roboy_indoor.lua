4c63adf3d05e15f4e6a3c6b35326a32ed9af8f02
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-23 20:49:15 +0100

    increased number of submaps

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -76,7 +76,7 @@ TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 1.
 TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.05
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(10.0)
 
-TRAJECTORY_BUILDER_2D.submaps.num_range_data = 15
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 10
 --TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1080
 
 -- TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.grid_type = 
