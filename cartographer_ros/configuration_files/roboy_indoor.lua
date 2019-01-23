70b142cc72beddc1eacde4950c05e01deeeeb252
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-23 21:14:23 +0100

    constraint threshold 60% 45°

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -68,7 +68,7 @@ TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.use_nonmonotonic_s
 
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.1
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(20.0)
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(10.0)
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 0.01
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight= 0.1 
 
@@ -87,13 +87,13 @@ POSE_GRAPH.optimize_every_n_nodes = 25
 
 POSE_GRAPH.constraint_builder.max_constraint_distance = 15
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 20
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(90.0)
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(45.0)
 
 POSE_GRAPH.constraint_builder.sampling_ratio = 0.1
 
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.branch_and_bound_depth = 6
 
-POSE_GRAPH.constraint_builder.min_score = 0.5
+POSE_GRAPH.constraint_builder.min_score = 0.6
 
 --POSE_GRAPH.constraint_builder.ceres_scan_matcher
 
