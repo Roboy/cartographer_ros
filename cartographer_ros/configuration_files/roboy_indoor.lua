085761283f2543600c194f6ce91694058a3b68fd
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-23 19:37:55 +0100

    various small

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -50,7 +50,7 @@ MAP_BUILDER.num_background_threads = 28
 TRAJECTORY_BUILDER_2D.min_range = 1
 TRAJECTORY_BUILDER_2D.max_range = 25
 
-TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 25
+TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 20
 
 TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.05
 TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_length = 3
@@ -73,7 +73,7 @@ TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight= 0.01 
 
 TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 1.
-TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.1
+TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.05
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(10.0)
 
 TRAJECTORY_BUILDER_2D.submaps.num_range_data = 10
@@ -85,7 +85,7 @@ TRAJECTORY_BUILDER_2D.submaps.num_range_data = 10
 -- -- GLOBAL -- --
 POSE_GRAPH.optimize_every_n_nodes = 25
 
-POSE_GRAPH.constraint_builder.max_constraint_distance = 10
+POSE_GRAPH.constraint_builder.max_constraint_distance = 25
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 10
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(160.0)
 
