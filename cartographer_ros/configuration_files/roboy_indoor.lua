e5c39480119e7b525187af823ddfb136d411c89c
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-23 19:42:30 +0100

    decreased global min score

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -93,7 +93,7 @@ POSE_GRAPH.constraint_builder.sampling_ratio = 0.05
 
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.branch_and_bound_depth = 6
 
-POSE_GRAPH.constraint_builder.min_score = 0.6
+POSE_GRAPH.constraint_builder.min_score = 0.4
 
 --POSE_GRAPH.constraint_builder.ceres_scan_matcher
 
