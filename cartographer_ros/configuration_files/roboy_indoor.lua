a35b4c90d2ebda830f34b3fc5a9441bc7e4238ca
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-24 17:25:08 +0100

    maxrange 30, submap resolution 0.1

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -47,7 +47,7 @@ MAP_BUILDER.num_background_threads = 28
 
 -- -- INPUT DATA -- --
 TRAJECTORY_BUILDER_2D.min_range = 0.25
-TRAJECTORY_BUILDER_2D.max_range = 25
+TRAJECTORY_BUILDER_2D.max_range = 30
 
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 20
 
@@ -67,7 +67,7 @@ TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.num_threads = 28
 
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.2
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(35.0)
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(20.0)
 --TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 1e-2
 --TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight= 1e-1
 
@@ -76,13 +76,15 @@ TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.05
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(1.0)
 
 TRAJECTORY_BUILDER_2D.submaps.num_range_data = 12
+TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.resolution = 0.1
+
 --TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1080
 
 -- TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.grid_type = 
 
 
 -- -- GLOBAL -- --
-POSE_GRAPH.optimize_every_n_nodes = 25
+POSE_GRAPH.optimize_every_n_nodes = 0
 
 POSE_GRAPH.constraint_builder.ceres_scan_matcher.ceres_solver_options.num_threads = 28
 
