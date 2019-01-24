dc31e55a036a620248d4f3c029b089bc027564e0
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-24 15:48:37 +0100

    submaps

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -49,7 +49,7 @@ MAP_BUILDER.num_background_threads = 28
 TRAJECTORY_BUILDER_2D.min_range = 1
 TRAJECTORY_BUILDER_2D.max_range = 25
 
-TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 25
+TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 20
 
 TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.05
 TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_length = 3
@@ -75,7 +75,7 @@ TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 1.
 TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.05
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(1.0)
 
-TRAJECTORY_BUILDER_2D.submaps.num_range_data = 15
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 10
 --TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1080
 
 -- TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.grid_type = 
