a13a493abccd53f26f680c6ce15ebe27a144800b
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-24 10:15:07 +0100

    motion filter on

--- cartographer_ros/configuration_files/roboy_outdoor.lua
+++ cartographer_ros/configuration_files/roboy_outdoor.lua
@@ -61,8 +61,8 @@ TRAJECTORY_BUILDER_2D.submaps.num_range_data = 10
 --TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight= 0.5
 
 --TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 1.
---TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.05
---TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(5.0)
+TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.05
+TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(5.0)
 
 -- TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.grid_type = 
 
