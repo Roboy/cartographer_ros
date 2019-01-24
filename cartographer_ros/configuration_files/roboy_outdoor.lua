eef869b3c460e924e00b07f79204bc3b2e243be5
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-24 10:33:32 +0100

    removed scan matching

--- cartographer_ros/configuration_files/roboy_outdoor.lua
+++ cartographer_ros/configuration_files/roboy_outdoor.lua
@@ -56,9 +56,9 @@ TRAJECTORY_BUILDER_2D.submaps.num_range_data = 10
 --TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 1e-7
 --TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 1e-8
 
-TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.1
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(10.0)
+--TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+--TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.1
+--TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(10.0)
 --TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 0.5
 --TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight= 0.5
 
