7f16814ccda946b2c6a9021aaa70bd3d04d8de7c
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-24 17:32:39 +0100

    ceres_solver_options.num_threads = 28

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -84,23 +84,19 @@ TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.resolution = 0.1
 
 
 -- -- GLOBAL -- --
-POSE_GRAPH.optimize_every_n_nodes = 0
+POSE_GRAPH.optimize_every_n_nodes = 25
 
 POSE_GRAPH.constraint_builder.ceres_scan_matcher.ceres_solver_options.num_threads = 28
+POSE_GRAPH.optimization_problem.ceres_solver_options.num_threads = 28
 
 POSE_GRAPH.constraint_builder.max_constraint_distance = 15
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 20
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(30.0)
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(15.0)
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.branch_and_bound_depth = 10
 
-POSE_GRAPH.constraint_builder.sampling_ratio = 0.05
-
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.branch_and_bound_depth = 6
-
-
+--POSE_GRAPH.constraint_builder.sampling_ratio = 0.003
 
 POSE_GRAPH.constraint_builder.min_score = 0.5
-
---POSE_GRAPH.constraint_builder.ceres_scan_matcher
 
 --POSE_GRAPH.constraint_builder.loop_closure_translation_weight = 1e-8
 --POSE_GRAPH.constraint_builder.loop_closure_rotation_weight = 1e-8
