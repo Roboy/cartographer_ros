f6dc8012dcc4bff2ad1e272a9e6807c6b2c4894c
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-24 12:23:47 +0100

    Update roboy_outdoor.lua

--- cartographer_ros/configuration_files/roboy_outdoor.lua
+++ cartographer_ros/configuration_files/roboy_outdoor.lua
@@ -54,8 +54,8 @@ TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 80
 
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.5
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(10.0)
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 1e-2
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(5.0)
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 1e-3
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight= 1e1
 
 TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 1
