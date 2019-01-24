0da1ddbe7027b7d7dfa7511599f6a2910031346f
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-24 16:21:16 +0100

    back to original, allowing larger voxel items

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -52,7 +52,7 @@ TRAJECTORY_BUILDER_2D.max_range = 25
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 20
 
 TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.05
-TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_length = 3
+TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_length = 5
 TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.min_num_points = 70
 
 TRAJECTORY_BUILDER_2D.use_imu_data = false
@@ -62,8 +62,8 @@ TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.use_nonmonotonic_steps = true
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.num_threads = 28
 --TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 20
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 5
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 40
+--TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 5
+--TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 40
 
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.2
@@ -75,14 +75,14 @@ TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 1.
 TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.05
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(1.0)
 
-TRAJECTORY_BUILDER_2D.submaps.num_range_data = 10
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 12
 --TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1080
 
 -- TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.grid_type = 
 
 
 -- -- GLOBAL -- --
-POSE_GRAPH.optimize_every_n_nodes = 25
+POSE_GRAPH.optimize_every_n_nodes = 0
 
 POSE_GRAPH.constraint_builder.max_constraint_distance = 10
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 10
