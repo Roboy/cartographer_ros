cf6bbe7b2cdd36d2b9dc07466f5753d81fd7d68f
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-24 12:52:59 +0100

    velocity worked, weigths back to init

--- cartographer_ros/configuration_files/roboy_outdoor.lua
+++ cartographer_ros/configuration_files/roboy_outdoor.lua
@@ -49,14 +49,14 @@ TRAJECTORY_BUILDER_2D.submaps.num_range_data = 3
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.use_nonmonotonic_steps = true
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.num_threads = 28
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 20
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 5
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 80
+--TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 10
+--TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 40
 
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 1
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(5.0)
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 1e-3
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight= 1e1
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.5
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(10.0)
+--TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 1e-2
+--TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight= 1e-1
 
 TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 1
 TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.01
