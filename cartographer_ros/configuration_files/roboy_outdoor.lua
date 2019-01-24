cc535c3a4207952addbbc9808e631692fe4c3bb2
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-24 16:05:49 +0100

    removed voxels

--- cartographer_ros/configuration_files/roboy_outdoor.lua
+++ cartographer_ros/configuration_files/roboy_outdoor.lua
@@ -35,14 +35,14 @@ MAP_BUILDER.num_background_threads = 28
  TRAJECTORY_BUILDER_2D.use_imu_data = false
 
 TRAJECTORY_BUILDER_2D.min_range = 0.25
-TRAJECTORY_BUILDER_2D.max_range = 50
+TRAJECTORY_BUILDER_2D.max_range = 40
 
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 100
 
-TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.025
-TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_range = 200
-TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_length = 5
-TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.min_num_points = 200
+--TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.025
+--TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_range = 200
+--TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_length = 5
+--TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.min_num_points = 200
 
 -- -- LOCAL -- --
 TRAJECTORY_BUILDER_2D.submaps.num_range_data = 3
@@ -54,7 +54,7 @@ TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 5
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 40
 
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.5
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.2
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(60.0)
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 1e-2
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight= 1e-1
@@ -63,9 +63,6 @@ TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 1
 TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.05
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(1.0)
 
--- TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.grid_type = 
-
-
 -- -- GLOBAL -- --
 POSE_GRAPH.optimize_every_n_nodes = 25
 
@@ -73,7 +70,7 @@ POSE_GRAPH.constraint_builder.min_score = 0.5
 POSE_GRAPH.constraint_builder.max_constraint_distance = 15
 
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 20
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(90.0)
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(60.0)
 
 --POSE_GRAPH.constraint_builder.sampling_ratio = 0.1
 
