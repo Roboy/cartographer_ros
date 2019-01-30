2763e27f6d4e654ef84d04d64110910aa55cb577
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-30 20:27:09 +0100

    angular_search_window = math.rad(70.0)

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -74,7 +74,7 @@ POSE_GRAPH.optimization_problem.ceres_solver_options.num_threads = 28
 
 POSE_GRAPH.constraint_builder.max_constraint_distance = 50
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 50
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(45.0)
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(70.0)
 --POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.branch_and_bound_depth = 20
 
 --POSE_GRAPH.constraint_builder.sampling_ratio = 0.003
