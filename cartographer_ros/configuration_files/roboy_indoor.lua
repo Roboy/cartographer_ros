f2ba55ad2fa10597fd934ee7eccfca64fb6d4cde
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-30 19:31:16 +0100

    expensive to move submaps

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -54,7 +54,7 @@ TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 2000
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.1
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(10.0)
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 1e-5
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 1e-1
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight= 1e-1
 
 TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 1.
