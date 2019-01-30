596c7ee65a7013e4e69b871830b4c7d4e7125438
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-30 20:12:28 +0100

    std branch and bound depth

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -75,7 +75,7 @@ POSE_GRAPH.optimization_problem.ceres_solver_options.num_threads = 28
 POSE_GRAPH.constraint_builder.max_constraint_distance = 15
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 50
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(15.0)
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.branch_and_bound_depth = 20
+--POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.branch_and_bound_depth = 20
 
 --POSE_GRAPH.constraint_builder.sampling_ratio = 0.003
 
