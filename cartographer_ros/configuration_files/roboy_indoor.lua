fe546a5f3a638ad666e0a62808339b07c34e373c
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-31 19:31:36 +0100

    Update roboy_indoor.lua

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -28,6 +28,40 @@ options = {
   landmarks_sampling_ratio = 1.,
 }
 
+TRAJECTORY_BUILDER_2D.min_range = 0.25
+TRAJECTORY_BUILDER_2D.max_range = 30
+
+TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 20
+
+TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.1
+TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_length = 10
+--TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.min_num_points = 70
+
+TRAJECTORY_BUILDER_2D.use_imu_data = true
+
+
+-- -- LOCAL -- --
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.use_nonmonotonic_steps = true
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.num_threads = 28
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 30
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 20
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 2000
+
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.2
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(20.0)
+--TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 1e-5
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight= 1e-1
+
+TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 5.
+TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.05
+TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(1.0)
+
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 12
+TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.resolution = 0.1
+
+--TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1080
+
 MAP_BUILDER.use_trajectory_builder_2d = true
 MAP_BUILDER.num_background_threads = 28
 
