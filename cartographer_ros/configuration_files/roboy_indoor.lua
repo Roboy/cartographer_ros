d04be6a5246d51ae134ecd399e23cc2bdaf682b1
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-31 15:55:46 +0100

    5cm & 10 scans

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -58,10 +58,10 @@ TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window =
 --TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight= 1e-5
 
 TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 5.
-TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.1
+TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.05
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(1.0)
 
-TRAJECTORY_BUILDER_2D.submaps.num_range_data = 12
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 10
 TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.resolution = 0.1
 
 --TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1080
