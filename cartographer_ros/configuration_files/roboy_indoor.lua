d58a85b2b37818dcfdc2158546e63cb90d86ef42
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-31 20:02:17 +0100

    Update roboy_indoor.lua

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -49,19 +49,19 @@ TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.use_nonmonotonic_s
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.num_threads = 28
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 30
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 10 --20
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 2e2 --80
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 2e4 --80
 
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.1
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(25.0)
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 1e-5
+--TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 1e-5
 --TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight= 1e-5
 
 TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 5.
 TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.05
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(1.0)
 
-TRAJECTORY_BUILDER_2D.submaps.num_range_data = 10
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 12
 TRAJECTORY_BUILDER_2D.submaps.grid_options_2d.resolution = 0.1
 
 --TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1080
