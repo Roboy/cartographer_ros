c179818def36e6144f288a6e30653259b6dfa561
Christoph <34374252+christqoh@users.noreply.github.com>
2019-01-31 19:51:31 +0100

    acceleration_weight = 1e5

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -89,8 +89,8 @@ POSE_GRAPH.constraint_builder.min_score = 0.65 --0.68
 --POSE_GRAPH.optimization_problem.ceres_solver_options
 
 POSE_GRAPH.optimization_problem.huber_scale = 1e1
-POSE_GRAPH.optimization_problem.acceleration_weight = 1e3
-POSE_GRAPH.optimization_problem.rotation_weight = 3e7
+POSE_GRAPH.optimization_problem.acceleration_weight = 1e5
+POSE_GRAPH.optimization_problem.rotation_weight = 3e5
 
 POSE_GRAPH.max_num_final_iterations = 10
 
