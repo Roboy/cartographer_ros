5fe1418193e1edfaa39cc9eed71ddbe72aabb1f9
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-15 18:01:53 +0100

    acceleration_weight = 1e7

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -72,8 +72,8 @@ POSE_GRAPH.optimization_problem.ceres_solver_options.use_nonmonotonic_steps = tr
 --    huber_scale = 1e1,
 --    acceleration_weight = 1e3,
 --    rotation_weight = 3e5,
---POSE_GRAPH.optimization_problem.huber_scale = 1e3
---POSE_GRAPH.optimization_problem.acceleration_weight = 1e1
+--POSE_GRAPH.optimization_problem.huber_scale = 1e2
+POSE_GRAPH.optimization_problem.acceleration_weight = 1e7
 --POSE_GRAPH.optimization_problem.rotation_weight = 3e4
 
 --    local_slam_pose_translation_weight = 1e5,
