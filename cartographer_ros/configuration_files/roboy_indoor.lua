6526b00650b9de88bde3ccf53caa8a1ad2302761
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-15 18:18:16 +0100

    acceleration_weight = 1e2

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -73,7 +73,7 @@ POSE_GRAPH.optimization_problem.ceres_solver_options.use_nonmonotonic_steps = tr
 --    acceleration_weight = 1e3,
 --    rotation_weight = 3e5,
 POSE_GRAPH.optimization_problem.huber_scale = 1e3
-POSE_GRAPH.optimization_problem.acceleration_weight = 1e7
+POSE_GRAPH.optimization_problem.acceleration_weight = 1e2
 --POSE_GRAPH.optimization_problem.rotation_weight = 3e4
 
 --    local_slam_pose_translation_weight = 1e5,
@@ -105,7 +105,6 @@ POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_windo
 POSE_GRAPH.constraint_builder.sampling_ratio = 0.1
 
 POSE_GRAPH.constraint_builder.min_score = 0.6 --0.68
---for large arease use .7, there are constraints above that number
 
 --POSE_GRAPH.constraint_builder.loop_closure_translation_weight = 1e-8
 --POSE_GRAPH.constraint_builder.loop_closure_rotation_weight = 1e-8
@@ -115,7 +114,7 @@ POSE_GRAPH.constraint_builder.min_score = 0.6 --0.68
 --  matcher_translation_weight = 5e2,
 --  matcher_rotation_weight = 1.6e3,
 --POSE_GRAPH.matcher_translation_weight = 1e-8
-POSE_GRAPH.matcher_rotation_weight = 1e3
+POSE_GRAPH.matcher_rotation_weight = 1e2
 
 --    loop_closure_translation_weight = 1.1e4,
 --    loop_closure_rotation_weight = 1e5,
