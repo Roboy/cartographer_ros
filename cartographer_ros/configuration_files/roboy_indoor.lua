39b2ff9fcfddc564e0bc34907dc80b44db614c55
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-15 18:10:14 +0100

    matcher_rotation_weight = 1e3

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -115,7 +115,7 @@ POSE_GRAPH.constraint_builder.min_score = 0.62 --0.68
 --  matcher_translation_weight = 5e2,
 --  matcher_rotation_weight = 1.6e3,
 --POSE_GRAPH.matcher_translation_weight = 1e-8
---POSE_GRAPH.matcher_rotation_weight = 1e-8
+POSE_GRAPH.matcher_rotation_weight = 1e3
 
 --    loop_closure_translation_weight = 1.1e4,
 --    loop_closure_rotation_weight = 1e5,
