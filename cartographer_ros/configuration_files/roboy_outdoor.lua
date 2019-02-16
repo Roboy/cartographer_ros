8db82bff6450c864d01a26369f208f2e56a153cb
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-16 14:30:35 +0100

    min_score = 0.5

--- cartographer_ros/configuration_files/roboy_outdoor.lua
+++ cartographer_ros/configuration_files/roboy_outdoor.lua
@@ -104,7 +104,7 @@ POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_windo
 
 --POSE_GRAPH.constraint_builder.sampling_ratio = 0.1
 
-POSE_GRAPH.constraint_builder.min_score = 0.6 --0.68
+POSE_GRAPH.constraint_builder.min_score = 0.5 --0.68
 
 -- LOOP CLOSURE --
 
