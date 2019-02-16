17bf30418fbf5f5e92eba25f1a330ebf75248a46
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-16 13:24:43 +0100

    POSE_GRAPH.matcher_rotation_weight = 1e2

--- cartographer_ros/configuration_files/roboy_outdoor.lua
+++ cartographer_ros/configuration_files/roboy_outdoor.lua
@@ -104,14 +104,14 @@ POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_windo
 
 --POSE_GRAPH.constraint_builder.sampling_ratio = 0.1
 
-POSE_GRAPH.constraint_builder.min_score = 0.55 --0.68
+POSE_GRAPH.constraint_builder.min_score = 0.6 --0.68
 
 -- LOOP CLOSURE --
 
 --  matcher_translation_weight = 5e2,
 --  matcher_rotation_weight = 1.6e3,
 --POSE_GRAPH.matcher_translation_weight = 1e2
---POSE_GRAPH.matcher_rotation_weight = 1e3
+POSE_GRAPH.matcher_rotation_weight = 1e2
 
 --    loop_closure_translation_weight = 1.1e4,
 --    loop_closure_rotation_weight = 1e5,
