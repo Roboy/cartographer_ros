c8a5cff113dd2473515606fb715ee71d458fca4a
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-16 13:14:41 +0100

    smaller loop closure weights

--- cartographer_ros/configuration_files/roboy_outdoor.lua
+++ cartographer_ros/configuration_files/roboy_outdoor.lua
@@ -106,9 +106,6 @@ POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_windo
 
 POSE_GRAPH.constraint_builder.min_score = 0.62 --0.68
 
---POSE_GRAPH.constraint_builder.loop_closure_translation_weight = 1e-8
---POSE_GRAPH.constraint_builder.loop_closure_rotation_weight = 1e-8
-
 -- LOOP CLOSURE --
 
 --  matcher_translation_weight = 5e2,
@@ -118,8 +115,8 @@ POSE_GRAPH.constraint_builder.min_score = 0.62 --0.68
 
 --    loop_closure_translation_weight = 1.1e4,
 --    loop_closure_rotation_weight = 1e5,
-POSE_GRAPH.constraint_builder.loop_closure_translation_weight = 1e2
-POSE_GRAPH.constraint_builder.loop_closure_rotation_weight = 1e3
+POSE_GRAPH.constraint_builder.loop_closure_translation_weight = 1e1
+POSE_GRAPH.constraint_builder.loop_closure_rotation_weight = 1e2
 
 
 POSE_GRAPH.max_num_final_iterations = 1
