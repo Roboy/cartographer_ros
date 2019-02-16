e6f2cd54bf3c621839c87138c341c5e23fd160ac
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-16 14:07:30 +0100

    local_slam_pose_rotation_weight = 1e4

--- cartographer_ros/configuration_files/roboy_outdoor.lua
+++ cartographer_ros/configuration_files/roboy_outdoor.lua
@@ -72,14 +72,14 @@ POSE_GRAPH.optimization_problem.ceres_solver_options.use_nonmonotonic_steps = tr
 --    huber_scale = 1e1,
 --    acceleration_weight = 1e3,
 --    rotation_weight = 3e5,
---POSE_GRAPH.optimization_problem.huber_scale = 1e3
+--POSE_GRAPH.optimization_problem.huber_scale = 1e2
 POSE_GRAPH.optimization_problem.acceleration_weight = 1e2
 POSE_GRAPH.optimization_problem.rotation_weight = 3e8
 
 --    local_slam_pose_translation_weight = 1e5,
 --    local_slam_pose_rotation_weight = 1e5,
---POSE_GRAPH.optimization_problem.local_slam_pose_translation_weight = 1e3
---POSE_GRAPH.optimization_problem.local_slam_pose_rotation_weight = 1e2
+--POSE_GRAPH.optimization_problem.local_slam_pose_translation_weight = 1e5
+POSE_GRAPH.optimization_problem.local_slam_pose_rotation_weight = 1e4
 
 --    odometry_translation_weight = 1e5,
 --    odometry_rotation_weight = 1e5,
