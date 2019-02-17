c7c117e8ebf7cb1a52a296c4414149919a457b4c
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-17 17:31:36 +0100

    Update roboy_outdoor.lua

--- cartographer_ros/configuration_files/roboy_outdoor.lua
+++ cartographer_ros/configuration_files/roboy_outdoor.lua
@@ -25,9 +25,9 @@ POSE_GRAPH.optimize_every_n_nodes = 200
 
 POSE_GRAPH.constraint_builder.ceres_scan_matcher.ceres_solver_options.num_threads = 28
 
-POSE_GRAPH.constraint_builder.max_constraint_distance = 3
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 50
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(90.0)
+POSE_GRAPH.constraint_builder.max_constraint_distance = 7
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 20
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(70.0)
 --POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.branch_and_bound_depth = 20
 
 --POSE_GRAPH.constraint_builder.sampling_ratio = 0.5
@@ -39,7 +39,7 @@ POSE_GRAPH.constraint_builder.min_score = 0.55 --0.44
 --  matcher_translation_weight = 5e2,
 --  matcher_rotation_weight = 1.6e3,
 POSE_GRAPH.matcher_translation_weight = 3e2
-POSE_GRAPH.matcher_rotation_weight = 2e3
+--POSE_GRAPH.matcher_rotation_weight = 2e3
 
 --    loop_closure_translation_weight = 1.1e4,
 --    loop_closure_rotation_weight = 1e5,
