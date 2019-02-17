05ad713768ad3fac4bb56d76bb17ad539d442aaf
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-17 18:07:21 +0100

    max_constraint_distance = 7

--- cartographer_ros/configuration_files/roboy_outdoor.lua
+++ cartographer_ros/configuration_files/roboy_outdoor.lua
@@ -25,7 +25,7 @@ POSE_GRAPH.optimize_every_n_nodes = 200
 
 POSE_GRAPH.constraint_builder.ceres_scan_matcher.ceres_solver_options.num_threads = 28
 
-POSE_GRAPH.constraint_builder.max_constraint_distance = 3
+POSE_GRAPH.constraint_builder.max_constraint_distance = 7
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 20
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(70.0)
 --POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.branch_and_bound_depth = 20
