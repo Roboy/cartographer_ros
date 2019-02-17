dba4bd738871d58139074b3ff2396ead7df32cc6
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-17 19:40:02 +0100

    matcher weights back

--- cartographer_ros/configuration_files/roboy_outdoor.lua
+++ cartographer_ros/configuration_files/roboy_outdoor.lua
@@ -27,7 +27,7 @@ POSE_GRAPH.constraint_builder.ceres_scan_matcher.ceres_solver_options.num_thread
 
 POSE_GRAPH.constraint_builder.max_constraint_distance = 3
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 50
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(90.0)
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(75.0)
 --POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.branch_and_bound_depth = 20
 
 --POSE_GRAPH.constraint_builder.sampling_ratio = 0.5
@@ -38,8 +38,8 @@ POSE_GRAPH.constraint_builder.min_score = 0.55 --0.44
 
 --  matcher_translation_weight = 5e2,
 --  matcher_rotation_weight = 1.6e3,
---POSE_GRAPH.matcher_translation_weight = 3e2
---POSE_GRAPH.matcher_rotation_weight = 1e3
+POSE_GRAPH.matcher_translation_weight = 3e2
+POSE_GRAPH.matcher_rotation_weight = 1.3e3
 
 --    loop_closure_translation_weight = 1.1e4,
 --    loop_closure_rotation_weight = 1e5,
