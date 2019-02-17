2721e5ec416d2a9d52b27c66150fb129c83d5f1b
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-17 17:54:07 +0100

    matcher_rotation_weight = 2e3

--- cartographer_ros/configuration_files/roboy_outdoor.lua
+++ cartographer_ros/configuration_files/roboy_outdoor.lua
@@ -39,7 +39,7 @@ POSE_GRAPH.constraint_builder.min_score = 0.5 --0.44
 --  matcher_translation_weight = 5e2,
 --  matcher_rotation_weight = 1.6e3,
 POSE_GRAPH.matcher_translation_weight = 3e2
-POSE_GRAPH.matcher_rotation_weight = 1.3e3
+POSE_GRAPH.matcher_rotation_weight = 2e3
 
 --    loop_closure_translation_weight = 1.1e4,
 --    loop_closure_rotation_weight = 1e5,
