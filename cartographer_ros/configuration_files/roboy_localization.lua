5052ab65375d203fb352062e43e2d62a1074bcfe
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-18 16:33:38 +0100

    smaller window but more constraints

--- cartographer_ros/configuration_files/roboy_localization.lua
+++ cartographer_ros/configuration_files/roboy_localization.lua
@@ -10,15 +10,15 @@ POSE_GRAPH.global_constraint_search_after_n_seconds = 0.1
 POSE_GRAPH.global_sampling_ratio = 0.001
 POSE_GRAPH.constraint_builder.sampling_ratio = 0.1
 
-POSE_GRAPH.constraint_builder.global_localization_min_score = 0.55
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.53
 --POSE_GRAPH.constraint_builder.min_score = 0.5
 
 
 -- global_sampling_ratio
 
-POSE_GRAPH.constraint_builder.max_constraint_distance = 20
+POSE_GRAPH.constraint_builder.max_constraint_distance = 15
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 25
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(90.0)
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(70.0)
 
 POSE_GRAPH.optimize_every_n_nodes = 2
 POSE_GRAPH.max_num_final_iterations = 1
