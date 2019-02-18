a9e3b4a09ebb3fa5544909f0df0cf4d28c8a69c0
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-18 15:36:14 +0100

    lets see if it sticks where it is after initialization

--- cartographer_ros/configuration_files/roboy_localization.lua
+++ cartographer_ros/configuration_files/roboy_localization.lua
@@ -13,8 +13,8 @@ POSE_GRAPH.constraint_builder.global_localization_min_score = 0.55
 -- global_sampling_ratio
 
 --POSE_GRAPH.constraint_builder.max_constraint_distance = 10
---POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 10
---POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(180.0)
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 5
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(25.0)
 
 POSE_GRAPH.optimize_every_n_nodes = 1
 
