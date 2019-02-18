3ba6bc5557dd9fef3485fe2b96b6d0c93e2e23d0
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-18 16:16:45 +0100

    lower sampling ratio for real time

--- cartographer_ros/configuration_files/roboy_localization.lua
+++ cartographer_ros/configuration_files/roboy_localization.lua
@@ -7,8 +7,8 @@ TRAJECTORY_BUILDER.pure_localization = true
 
 POSE_GRAPH.global_constraint_search_after_n_seconds = 0
 
---POSE_GRAPH.global_sampling_ratio = 0.003
---POSE_GRAPH.constraint_builder.sampling_ratio = 1
+POSE_GRAPH.global_sampling_ratio = 0.0003
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.1
 
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.55
 --POSE_GRAPH.constraint_builder.min_score = 0.5
