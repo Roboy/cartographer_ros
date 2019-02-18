41455c154ba9e4b6dca90bb5069506712d4e914f
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-18 16:14:28 +0100

    large search window

--- cartographer_ros/configuration_files/roboy_localization.lua
+++ cartographer_ros/configuration_files/roboy_localization.lua
@@ -16,9 +16,9 @@ POSE_GRAPH.constraint_builder.global_localization_min_score = 0.5
 
 -- global_sampling_ratio
 
---POSE_GRAPH.constraint_builder.max_constraint_distance = 5
---POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 5
---POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(45.0)
+POSE_GRAPH.constraint_builder.max_constraint_distance = 10
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 25
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(45.0)
 
 POSE_GRAPH.optimize_every_n_nodes = 3
 POSE_GRAPH.max_num_final_iterations = 1
