5e55cf7f813b2f63e6cc98c39060e0df1b8b1c65
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-19 19:33:09 +0100

    sampling increase

--- cartographer_ros/configuration_files/roboy_localization.lua
+++ cartographer_ros/configuration_files/roboy_localization.lua
@@ -5,10 +5,10 @@ TRAJECTORY_BUILDER.pure_localization = true
 --	max_submaps_to_keep = 3,
 --}
 
-POSE_GRAPH.global_constraint_search_after_n_seconds = 8
+POSE_GRAPH.global_constraint_search_after_n_seconds = 6
 
-POSE_GRAPH.global_sampling_ratio = 0.008
---POSE_GRAPH.constraint_builder.sampling_ratio = 0.5
+POSE_GRAPH.global_sampling_ratio = 0.005
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.5
 
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.5
 --POSE_GRAPH.constraint_builder.min_score = 0.4
@@ -16,7 +16,7 @@ POSE_GRAPH.constraint_builder.global_localization_min_score = 0.5
 
 -- global_sampling_ratio
 
-POSE_GRAPH.constraint_builder.max_constraint_distance = 10
+--POSE_GRAPH.constraint_builder.max_constraint_distance = 10
 --POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 25
 --POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(25.0)
 
