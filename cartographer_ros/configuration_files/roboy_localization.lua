1e63147b8e81f0bdb651cda9ebe25a8774a5d183
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-19 19:51:23 +0100

    global_localization_min_score = 0.2

--- cartographer_ros/configuration_files/roboy_localization.lua
+++ cartographer_ros/configuration_files/roboy_localization.lua
@@ -10,7 +10,7 @@ POSE_GRAPH.global_constraint_search_after_n_seconds = 6
 POSE_GRAPH.global_sampling_ratio = 0.008
 --POSE_GRAPH.constraint_builder.sampling_ratio = 0.5
 
-POSE_GRAPH.constraint_builder.global_localization_min_score = 0.25
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.2
 --POSE_GRAPH.constraint_builder.min_score = 0.4
 
 
