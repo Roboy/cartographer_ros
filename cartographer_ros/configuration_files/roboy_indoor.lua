cb5be606715a7c5f680ac23d424cc015ce858d22
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-19 15:48:34 +0100

    angular_search_window = math.rad(10.0)

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -54,7 +54,7 @@ TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 2e3 --80
 
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.2
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(20.0)
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(10.0)
 --TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 0.01
 --TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight= 0.01 
 
