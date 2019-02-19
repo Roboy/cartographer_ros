bca6806ec56206c48403d282fa0ed05d491bcd78
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-19 21:27:02 +0100

    20 nodes and 15%

--- cartographer_ros/configuration_files/roboy_localization.lua
+++ cartographer_ros/configuration_files/roboy_localization.lua
@@ -4,7 +4,7 @@ TRAJECTORY_BUILDER.pure_localization = true
 
 -- -- GLOBAL -- --
 POSE_GRAPH.global_constraint_search_after_n_seconds = 5
-POSE_GRAPH.constraint_builder.global_localization_min_score = 0.1
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.15
 POSE_GRAPH.global_sampling_ratio = 0.008
 
 -- -- LOCAL -- -- 
@@ -15,7 +15,7 @@ POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(15.0)
 
 -- -- OPTIMIZATION -- --
-POSE_GRAPH.optimize_every_n_nodes = 5
+POSE_GRAPH.optimize_every_n_nodes = 20
 --POSE_GRAPH.max_num_final_iterations = 1
 
 return options
