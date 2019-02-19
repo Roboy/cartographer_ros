fa95e74633fa6cc07311434e04e8e7a9da252358
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-19 18:42:25 +0100

    longer constraints

--- cartographer_ros/configuration_files/roboy_localization.lua
+++ cartographer_ros/configuration_files/roboy_localization.lua
@@ -16,9 +16,9 @@ POSE_GRAPH.constraint_builder.min_score = 0.4
 
 -- global_sampling_ratio
 
-POSE_GRAPH.constraint_builder.max_constraint_distance = 5
---POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 25
---POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(45.0)
+POSE_GRAPH.constraint_builder.max_constraint_distance = 10
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 25
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(25.0)
 
 POSE_GRAPH.optimize_every_n_nodes = 20
 POSE_GRAPH.max_num_final_iterations = 1
