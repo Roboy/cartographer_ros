97993dd42271212d733a160cf24779f99bcb2b9d
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-19 20:01:45 +0100

    Update roboy_localization.lua

--- cartographer_ros/configuration_files/roboy_localization.lua
+++ cartographer_ros/configuration_files/roboy_localization.lua
@@ -5,7 +5,7 @@ TRAJECTORY_BUILDER.pure_localization = true
 --	max_submaps_to_keep = 3,
 --}
 
-POSE_GRAPH.global_constraint_search_after_n_seconds = 7
+POSE_GRAPH.global_constraint_search_after_n_seconds = 5
 
 POSE_GRAPH.global_sampling_ratio = 0.008
 --POSE_GRAPH.constraint_builder.sampling_ratio = 0.5
@@ -20,7 +20,7 @@ POSE_GRAPH.constraint_builder.max_constraint_distance = 10
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 15
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(15.0)
 
-POSE_GRAPH.optimize_every_n_nodes = 10
+POSE_GRAPH.optimize_every_n_nodes = 25
 --POSE_GRAPH.max_num_final_iterations = 1
 
 return options
