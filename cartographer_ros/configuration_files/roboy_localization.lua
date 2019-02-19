007a89ce7d55af55facb41007f0306f229f3ae1e
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-19 18:39:05 +0100

    global_localization_min_score = 0.5

--- cartographer_ros/configuration_files/roboy_localization.lua
+++ cartographer_ros/configuration_files/roboy_localization.lua
@@ -8,17 +8,17 @@ TRAJECTORY_BUILDER.pure_localization = true
 POSE_GRAPH.global_constraint_search_after_n_seconds = 0.1
 
 --POSE_GRAPH.global_sampling_ratio = 0.001
-POSE_GRAPH.constraint_builder.sampling_ratio = 0.1
+--POSE_GRAPH.constraint_builder.sampling_ratio = 0.1
 
-POSE_GRAPH.constraint_builder.global_localization_min_score = 0.58
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.5
 --POSE_GRAPH.constraint_builder.min_score = 0.5
 
 
 -- global_sampling_ratio
 
-POSE_GRAPH.constraint_builder.max_constraint_distance = 15
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 25
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(45.0)
+--POSE_GRAPH.constraint_builder.max_constraint_distance = 15
+--POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 25
+--POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(45.0)
 
 POSE_GRAPH.optimize_every_n_nodes = 20
 POSE_GRAPH.max_num_final_iterations = 1
