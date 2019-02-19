8526bb21c7d031b47d60410a08a24d0276875144
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-19 16:10:12 +0100

    scan matcher weights stdt

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -51,8 +51,8 @@ POSE_GRAPH.optimization_problem.rotation_weight = 1e4
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.use_nonmonotonic_steps = true
 --TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 20
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.num_threads = 28
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 10 --20
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 2e3 --80
+--TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 10 --20
+--TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 2e3 --80
 
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = false
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.1
