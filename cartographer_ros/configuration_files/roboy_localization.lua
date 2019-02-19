8757c163c44decd52e43c7e45a72d669ea659262
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-19 21:27:41 +0100

    25 but more local constraints

--- cartographer_ros/configuration_files/roboy_localization.lua
+++ cartographer_ros/configuration_files/roboy_localization.lua
@@ -9,13 +9,13 @@ POSE_GRAPH.global_sampling_ratio = 0.008
 
 -- -- LOCAL -- -- 
 POSE_GRAPH.constraint_builder.sampling_ratio = 0.4
---POSE_GRAPH.constraint_builder.min_score = 0.5
+POSE_GRAPH.constraint_builder.min_score = 0.5
 POSE_GRAPH.constraint_builder.max_constraint_distance = 10
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 15
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(15.0)
 
 -- -- OPTIMIZATION -- --
-POSE_GRAPH.optimize_every_n_nodes = 20
+POSE_GRAPH.optimize_every_n_nodes = 25
 --POSE_GRAPH.max_num_final_iterations = 1
 
 return options
