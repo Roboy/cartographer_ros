420afb9019400f00d673fbd4738f198b92be00ef
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-19 19:41:25 +0100

    widnow 15 both

--- cartographer_ros/configuration_files/roboy_localization.lua
+++ cartographer_ros/configuration_files/roboy_localization.lua
@@ -17,8 +17,8 @@ POSE_GRAPH.constraint_builder.global_localization_min_score = 0.35
 -- global_sampling_ratio
 
 POSE_GRAPH.constraint_builder.max_constraint_distance = 10
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 20
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(10.0)
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 15
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(15.0)
 
 POSE_GRAPH.optimize_every_n_nodes = 20
 --POSE_GRAPH.max_num_final_iterations = 1
