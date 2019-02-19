d83d48ef957159f3805af16f1431ee4c62ee9298
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-19 20:31:09 +0100

    cleaning up

--- cartographer_ros/configuration_files/roboy_localization.lua
+++ cartographer_ros/configuration_files/roboy_localization.lua
@@ -1,25 +1,20 @@
 include "roboy_indoor.lua"
 
 TRAJECTORY_BUILDER.pure_localization = true
---TRAJECTORY_BUILDER.pure_localization_trimmer = {
---	max_submaps_to_keep = 3,
---}
 
+-- -- GLOBAL -- --
 POSE_GRAPH.global_constraint_search_after_n_seconds = 5
-
-POSE_GRAPH.global_sampling_ratio = 0.008
-POSE_GRAPH.constraint_builder.sampling_ratio = 0.4
-
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.18
+POSE_GRAPH.global_sampling_ratio = 0.008
+
+-- -- LOCAL -- -- 
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.4
 --POSE_GRAPH.constraint_builder.min_score = 0.4
-
-
--- global_sampling_ratio
-
 POSE_GRAPH.constraint_builder.max_constraint_distance = 10
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 15
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(15.0)
 
+-- -- OPTIMIZATION -- --
 POSE_GRAPH.optimize_every_n_nodes = 25
 --POSE_GRAPH.max_num_final_iterations = 1
 
