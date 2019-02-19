d56c04bcf182c32acf016cc7fa6b2dec91f388d5
Christoph <34374252+christqoh@users.noreply.github.com>
2019-02-19 21:28:43 +0100

    back to working I hope

--- cartographer_ros/configuration_files/roboy_localization.lua
+++ cartographer_ros/configuration_files/roboy_localization.lua
@@ -4,12 +4,12 @@ TRAJECTORY_BUILDER.pure_localization = true
 
 -- -- GLOBAL -- --
 POSE_GRAPH.global_constraint_search_after_n_seconds = 5
-POSE_GRAPH.constraint_builder.global_localization_min_score = 0.15
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.2
 POSE_GRAPH.global_sampling_ratio = 0.008
 
 -- -- LOCAL -- -- 
 POSE_GRAPH.constraint_builder.sampling_ratio = 0.4
-POSE_GRAPH.constraint_builder.min_score = 0.5
+--POSE_GRAPH.constraint_builder.min_score = 0.5
 POSE_GRAPH.constraint_builder.max_constraint_distance = 10
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 15
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(15.0)
