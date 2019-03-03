5d734d42479e42905a5c79a74f12dd7effc66a41
Christoph <34374252+christqoh@users.noreply.github.com>
2019-03-03 20:05:10 +0100

    less but longer constraints

--- cartographer_ros/configuration_files/roboy_outdoor.lua
+++ cartographer_ros/configuration_files/roboy_outdoor.lua
@@ -25,14 +25,14 @@ POSE_GRAPH.optimize_every_n_nodes = 100
 
 POSE_GRAPH.constraint_builder.ceres_scan_matcher.ceres_solver_options.num_threads = 28
 
-POSE_GRAPH.constraint_builder.max_constraint_distance = 5
+POSE_GRAPH.constraint_builder.max_constraint_distance = 10
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 60
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(75.0)
 --POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.branch_and_bound_depth = 20
 
 --POSE_GRAPH.constraint_builder.sampling_ratio = 0.5
 
-POSE_GRAPH.constraint_builder.min_score = 0.4--0.44
+POSE_GRAPH.constraint_builder.min_score = 0.5--0.44
 
 -- LOOP CLOSURE --
 
