308c19f0b61ac054ff69df5c4a04cf74672f949b
Christoph <34374252+christqoh@users.noreply.github.com>
2019-03-03 20:13:10 +0100

    increas roation weight, decreased acc weight

--- cartographer_ros/configuration_files/roboy_outdoor.lua
+++ cartographer_ros/configuration_files/roboy_outdoor.lua
@@ -12,8 +12,8 @@ POSE_GRAPH.optimization_problem.ceres_solver_options.use_nonmonotonic_steps = tr
 --    acceleration_weight = 1e3,
 --    rotation_weight = 3e5,
 --POSE_GRAPH.optimization_problem.huber_scale = 1e-1
-POSE_GRAPH.optimization_problem.acceleration_weight = 1e4
-POSE_GRAPH.optimization_problem.rotation_weight = 8e5
+POSE_GRAPH.optimization_problem.acceleration_weight = 1e2
+POSE_GRAPH.optimization_problem.rotation_weight = 1e4
 
 --    local_slam_pose_translation_weight = 1e5,
 --    local_slam_pose_rotation_weight = 1e5,
