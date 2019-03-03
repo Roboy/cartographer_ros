27c9373fc1b5db8275403cf594a3d859def9c59f
Christoph <34374252+christqoh@users.noreply.github.com>
2019-03-03 14:42:10 +0100

    TRAJECTORY_BUILDER_2D.min_range = 0.25

--- cartographer_ros/configuration_files/roboy_indoor.lua
+++ cartographer_ros/configuration_files/roboy_indoor.lua
@@ -33,7 +33,7 @@ MAP_BUILDER.use_trajectory_builder_2d = true
 MAP_BUILDER.num_background_threads = 28
 
 -- -- INPUT DATA -- --
-TRAJECTORY_BUILDER_2D.min_range = 1
+TRAJECTORY_BUILDER_2D.min_range = 0.25
 TRAJECTORY_BUILDER_2D.max_range = 25
 
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 20
