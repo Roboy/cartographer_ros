83b1ea6dd10e7ff002e034971d72f401fdce85bc
Christoph <34374252+christqoh@users.noreply.github.com>
2019-03-03 18:34:50 +0100

    max_range = 35

--- cartographer_ros/configuration_files/rickshaw_local.lua
+++ cartographer_ros/configuration_files/rickshaw_local.lua
@@ -30,7 +30,7 @@ MAP_BUILDER.num_background_threads = 28
 
 -- -- INPUT DATA -- --
 TRAJECTORY_BUILDER_2D.min_range = 0.25
-TRAJECTORY_BUILDER_2D.max_range = 25
+TRAJECTORY_BUILDER_2D.max_range = 35
 
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 20
 
