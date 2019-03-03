4a4db94cb81cde8f8af4ac461abe22909a2546cf
Christoph <34374252+christqoh@users.noreply.github.com>
2019-03-03 18:47:28 +0100

    imu stdt more constraints

--- cartographer_ros/configuration_files/roboy_outdoor.lua
+++ cartographer_ros/configuration_files/roboy_outdoor.lua
@@ -12,8 +12,8 @@ POSE_GRAPH.optimization_problem.ceres_solver_options.use_nonmonotonic_steps = tr
 --    acceleration_weight = 1e3,
 --    rotation_weight = 3e5,
 --POSE_GRAPH.optimization_problem.huber_scale = 1e-1
---POSE_GRAPH.optimization_problem.acceleration_weight = 1e2
-POSE_GRAPH.optimization_problem.rotation_weight = 1e6
+-- POSE_GRAPH.optimization_problem.acceleration_weight = 1e2
+-- POSE_GRAPH.optimization_problem.rotation_weight = 1e6
 
 --    local_slam_pose_translation_weight = 1e5,
 --    local_slam_pose_rotation_weight = 1e5,
@@ -32,7 +32,7 @@ POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_windo
 
 --POSE_GRAPH.constraint_builder.sampling_ratio = 0.5
 
-POSE_GRAPH.constraint_builder.min_score = 0.44--0.44
+POSE_GRAPH.constraint_builder.min_score = 0.4--0.44
 
 -- LOOP CLOSURE --
 
