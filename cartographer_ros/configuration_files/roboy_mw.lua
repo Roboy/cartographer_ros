e0958a1c269099efba28a1b7bb11b8efa55b8306
Christoph <34374252+christqoh@users.noreply.github.com>
2019-03-05 11:43:48 +0100

    min_score = 0.65

--- cartographer_ros/configuration_files/roboy_mw.lua
+++ cartographer_ros/configuration_files/roboy_mw.lua
@@ -43,7 +43,7 @@ POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_windo
 --POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.branch_and_bound_depth = 6
 
 POSE_GRAPH.constraint_builder.sampling_ratio = 0.7
-POSE_GRAPH.constraint_builder.min_score = 0.67
+POSE_GRAPH.constraint_builder.min_score = 0.65
 
 --POSE_GRAPH.constraint_builder.ceres_scan_matcher
 
