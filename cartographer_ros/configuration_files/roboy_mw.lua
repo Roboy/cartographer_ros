3925278e0dff4ea65597d361a8dd61336569c905
Christoph <34374252+christqoh@users.noreply.github.com>
2019-03-05 11:47:33 +0100

    voxel filter standard

--- cartographer_ros/configuration_files/roboy_mw.lua
+++ cartographer_ros/configuration_files/roboy_mw.lua
@@ -5,9 +5,9 @@ include "rickshaw_local.lua"
 -- -- LOCAL -- --
 TRAJECTORY_BUILDER_2D.max_range = 50
 
-TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.05
-TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_length = 3
-TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.min_num_points = 70
+--TRAJECTORY_BUILDER_2D.voxel_filter_size = 0.05
+--TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.max_length = 3
+--TRAJECTORY_BUILDER_2D.adaptive_voxel_filter.min_num_points = 70
 
 --TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 5 --20
 
