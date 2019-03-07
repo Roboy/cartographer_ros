e251d106472fb51021068cb279bbbfe372e1899b
Christoph <34374252+christqoh@users.noreply.github.com>
2019-03-07 10:20:06 +0100

    less rotation weight

--- cartographer_ros/configuration_files/roboy_mw.lua
+++ cartographer_ros/configuration_files/roboy_mw.lua
@@ -34,7 +34,7 @@ POSE_GRAPH.constraint_builder.min_score = 0.64 -- 0.53 -- 0.59
 --    loop_closure_translation_weight = 1.1e4
 --    loop_closure_rotation_weight = 1e5
 POSE_GRAPH.constraint_builder.loop_closure_translation_weight = 8e3 --9e3
---POSE_GRAPH.constraint_builder.loop_closure_rotation_weight = 1e4
+POSE_GRAPH.constraint_builder.loop_closure_rotation_weight = 8e4
 
 --  matcher_translation_weight = 5e2,
 --  matcher_rotation_weight = 1.6e3,
