c9e21de71bad05fe924948116e2f86b929d6b130
Christoph <34374252+christqoh@users.noreply.github.com>
2019-03-07 13:37:51 +0100

    optimize_every_n_nodes = 10

--- cartographer_ros/configuration_files/roboy_mw_localization.lua
+++ cartographer_ros/configuration_files/roboy_mw_localization.lua
@@ -20,7 +20,7 @@ POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window
 POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(15.0)
 
 -- -- OPTIMIZATION -- --
-POSE_GRAPH.optimize_every_n_nodes = 20
+POSE_GRAPH.optimize_every_n_nodes = 10
 --POSE_GRAPH.max_num_final_iterations = 1
 
 return options
