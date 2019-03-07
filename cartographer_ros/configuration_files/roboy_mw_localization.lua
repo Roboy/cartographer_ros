1a09f5d4285a8103a7a7d0dfd2a71496c9852e0d
roboy <roboy>
2019-03-07 15:29:53 +0000

    fixes

--- cartographer_ros/configuration_files/roboy_mw_localization.lua
+++ cartographer_ros/configuration_files/roboy_mw_localization.lua
@@ -2,25 +2,28 @@ include "roboy_mw.lua"
 
 TRAJECTORY_BUILDER.pure_localization = true
 
+TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+
 -- -- GLOBAL -- --
 -- std:
 --     global_constraint_search_after_n_seconds = 10.,
 --     global_localization_min_score = 0.6
 --     global_sampling_ratio = 0.003,
 
-POSE_GRAPH.global_constraint_search_after_n_seconds = 10
-POSE_GRAPH.constraint_builder.global_localization_min_score = 0.55
---POSE_GRAPH.global_sampling_ratio = 0.005
+POSE_GRAPH.global_constraint_search_after_n_seconds = 7
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
+POSE_GRAPH.global_sampling_ratio = 0.005
 
 -- -- LOCAL -- -- 
 --POSE_GRAPH.constraint_builder.sampling_ratio = 0.6
---POSE_GRAPH.constraint_builder.min_score = 0.45
-POSE_GRAPH.constraint_builder.max_constraint_distance = 10
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 25
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(180.0)
+POSE_GRAPH.constraint_builder.min_score = 0.48
+POSE_GRAPH.constraint_builder.max_constraint_distance = 8
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 10
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(360.0)
 
 -- -- OPTIMIZATION -- --
-POSE_GRAPH.optimize_every_n_nodes = 10
---POSE_GRAPH.max_num_final_iterations = 1
+POSE_GRAPH.optimize_every_n_nodes = 5
+POSE_GRAPH.max_num_final_iterations = 1
 
 return options
