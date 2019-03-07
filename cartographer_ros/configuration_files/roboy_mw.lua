1d02b71281e375f4ab027f0118def87251d2e1a5
Christoph <34374252+christqoh@users.noreply.github.com>
2019-03-07 13:30:33 +0100

    matcher_translation_weight = 8e2

--- cartographer_ros/configuration_files/roboy_mw.lua
+++ cartographer_ros/configuration_files/roboy_mw.lua
@@ -38,7 +38,7 @@ POSE_GRAPH.constraint_builder.loop_closure_rotation_weight = 8e4
 
 --  matcher_translation_weight = 5e2,
 --  matcher_rotation_weight = 1.6e3,
-POSE_GRAPH.matcher_translation_weight = 1e2 --8e2
+POSE_GRAPH.matcher_translation_weight = 8e2 --8e2
 --POSE_GRAPH.matcher_rotation_weight = 1e3 --3e3
 
 --POSE_GRAPH.max_num_final_iterations = 1
